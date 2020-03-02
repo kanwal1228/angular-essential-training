49c5c62ead8817202fabccaa7b7bef37fd60fe82
Kanwal Preet Singh <kanwalpreet1228@gmail.com>
2020-03-02 15:12:44 -0600

    all changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 
 })
-export class AppComponent {}
+export class AppComponent {
+  firstMediaItem = {
+    id: 1,
+    name: 'Firebug',
+    medium: 'Series',
+    category: 'Science Fiction',
+    year: 2010,
+    watchedOn: 12944166565384,
+    isFavorite: false
+  };
+
+  onMediaItemDelete() {}
+}
--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'mw-media-item',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
 })
 
 export class MediaItemComponent {
+    @Input() mediaItem: any;
+    @Output() delete = new EventEmitter();
 
+    onDelete() {
+        console.log('deleted');
+        this.delete.emit(this.mediaItem);
+    }
 }
